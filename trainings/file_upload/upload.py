0dc6ee00d433d302dcff5fcbf31f0c971a6e0ad1
Michał Owczarek <michalzdw@gmail.com>
2014-10-15 22:03:16 +0200

    Added insert points of route

--- trainings/file_upload/upload.py
+++ trainings/file_upload/upload.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime
 from django.conf import settings
 from django.contrib.auth.models import User
-from trainings.models import Training
+from trainings.models import Training, Track
 import json
 
 def save_file(f):
@@ -17,6 +17,10 @@ def save_file(f):
     login = data['login']
     u = User.objects.get(username=login)
     print(data["points"][0]["time"], data["points"][-1]["time"], (data["points"][0]["time"]-data["points"][-1]["time"]))
-    training = Training.objects.create(date_start=datetime.utcfromtimestamp(data["points"][-1]["time"]/1000),
-                                       date_end=datetime.utcfromtimestamp(data["points"][0]["time"]/1000),
-                                       duration=(data["points"][0]["time"]-data["points"][-1]["time"]),user=u)
+    training = Training.objects.create(date_start=datetime.utcfromtimestamp(data["points"][0]["time"]/1000),
+                                       date_end=datetime.utcfromtimestamp(data["points"][-1]["time"]/1000),
+                                       duration=(data["points"][-1]["time"]-data["points"][0]["time"]),user=u)
+    last_id = Training.objects.latest('id')
+    for point in data['points']:
+        print point
+        Track.objects.create(training=last_id, lat=point['lat'], lng=point['lng'], time=point['time'])
